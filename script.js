1b40724944c45f139b1bec11b8cc48602c21a35e
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2023-05-02 10:22:58 -0700

    Rewrite to fix bugs

--- script.js
+++ script.js
@@ -1,185 +1,246 @@
 function Cell() {
-    let value = "";
-
-    const addToken = (token) => {
-        value = token;
-    }
+    let value = ''
 
     const getValue = () => value
 
+    const addToken = (token) => {
+        value = token
+    }
+
     return {
-        addToken,
-        getValue
+        getValue,
+        addToken
     }
 }
 
 function Gameboard() {
-    const rows = 3
-    const columns = 3
-    const board = []
+    let board = []
+    const numOfCells = 9
 
-    const getBoard = () => board
+    const clearBoard = () => {
+        board = []
+        for (let i = 0; i < numOfCells; i++) {
+            board.push(Cell())
+        }
+    }
 
-    const makeMove = (row, column, token) => {
-        const cell = board[row][column].getValue() ? console.log('invalid cell') : board[row][column]
+    const makeMove = (cellIdx, token) => {
+        const cell = board[cellIdx].getValue() ? false : board[cellIdx]
 
         if (!cell) return false
-        
+
         cell.addToken(token)
 
         return true
     }
 
-    const clearBoard = () => {
-        for (let i = 0; i < rows; i++) {
-            board[i] = []
-            for (let j = 0; j < columns; j++) {
-                board[i].push(Cell())
-            }
-        }
-    }
+    const getBoard = () => board
 
     clearBoard()
 
     return {
-        getBoard,
+        clearBoard,
         makeMove,
-        clearBoard
+        getBoard
     }
 }
 
-
-
-function GameController(
-    playerOneName = "Player One",
-    playerTwoName = "Player Two"
-) {
-
+function GameController(p1Name = 'Player One', p2Name = 'Player Two') {
     const board = Gameboard()
 
     const players = [
         {
-            name: playerOneName,
-            token: "X"
+            name: p1Name,
+            token: 'X'
         },
         {
-            name: playerTwoName,
-            token: "O"
+            name: p2Name,
+            token: 'O'
         }
-    ];
+    ]
 
-    let activePlayer = players[0];
+    let activePlayer = players[0]
 
-    const toggleMenu = () => {
-        const form = document.querySelector('.names')
-        form.classList.toggle('hidden')
+
+
+    const switchPlayer = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0]
+    }
+
+    const getActivePlayer = () => activePlayer
+
+    const checkWin = (token) => {
+        const winCons = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontals
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // verticals
+            [0, 4, 8], [6, 4, 2] // diagonals
+        ]
+
+        const numOfConditions = winCons.length
+        for (let i = 0; i < numOfConditions; i++) {
+            let currCon = false
+            for (let j = 0; j < 3; j++) {
+                let currCell = winCons[i][j]
+                if (board.getBoard()[currCell].getValue() === token) {
+                    currCon = true    
+                } else {
+                    currCon = false
+                    break
+                }
+            }
+            if (currCon) {
+                return true
+            }
+        }
+    }
+
+    const checkDraw = () => {
+        for (let i = 0; i < 9;) {
+            if (board.getBoard()[i].getValue()) {
+                i++
+                if (i === 9) {
+                    return true
+                }
+                continue
+            } else {
+                return false
+            }
+        }
+
+    }
+
+    const checkGameStatus = () => {
+        const token = getActivePlayer().token
+        let win = false
+        let draw = false
+        if (checkWin(token)) {
+            win = true
+        } else if (checkDraw()) {
+            draw = true
+        }
+        if (win) return 'win'
+        else if (draw) return 'draw'
+        return false
+    }
+    
+    const playRound = (cellIdx) => {
+        if (cellIdx > 8 || cellIdx < 0) return false
+        const token = getActivePlayer().token
+
+        if (!board.makeMove(cellIdx, token)) {
+            console.log('invalid cell')
+            return `${getActivePlayer().name}'s turn`
+        }
+
+        if (checkGameStatus() === 'win') {
+            return 'win'
+        } else if (checkGameStatus() === 'draw') {
+            return 'draw'
+        }
+        switchPlayer()
+        console.log(
+            board.getBoard()[0].getValue() ? board.getBoard()[0].getValue() : '-', 
+            board.getBoard()[1].getValue() ? board.getBoard()[1].getValue() : '-', 
+            board.getBoard()[2].getValue() ? board.getBoard()[2].getValue() : '-'
+        )
+        console.log(
+            board.getBoard()[3].getValue() ? board.getBoard()[3].getValue() : '-', 
+            board.getBoard()[4].getValue() ? board.getBoard()[4].getValue() : '-', 
+            board.getBoard()[5].getValue() ? board.getBoard()[5].getValue() : '-'
+        )
+        console.log(
+            board.getBoard()[6].getValue() ? board.getBoard()[6].getValue() : '-', 
+            board.getBoard()[7].getValue() ? board.getBoard()[7].getValue() : '-', 
+            board.getBoard()[8].getValue() ? board.getBoard()[8].getValue() : '-'
+        )
+        return `${getActivePlayer().name}'s turn`
+    }
+
+    return {
+        checkGameStatus,
+        playRound,
+        getActivePlayer,
+        getBoard: board.getBoard,
+        players
+    }
+    
+}
+
+
+function ScreenController() {
+    const game = GameController()
+    const heading = document.querySelector('.turn')
+    const boardDisplay = document.querySelector('.board')
+    const numOfCells = 9
+
+    const updateBoard = (board) => {
+        for (let i = 0; i < numOfCells; i++) {
+            const cell = document.createElement('button')
+            cell.classList.add('cell')
+            if (board[i].getValue() === 'X') {
+                cell.classList.add('X')
+            } else if (board[i].getValue() === 'O') {
+                cell.classList.add('O')
+            }
+            cell.dataset.num = i
+            cell.textContent = board[i].getValue()
+            boardDisplay.appendChild(cell)
+        }
+    }
+
+    const updateScreen = () => {
+        boardDisplay.textContent = ''
+        const board = game.getBoard()
+        const activePlayer = game.getActivePlayer().name
+
+        heading.textContent = `${activePlayer}'s Turn`
+        updateBoard(board)
     }
 
     const setPlayerNames = () => {
         const playerOne = document.getElementById('playerOne')
         const playerTwo = document.getElementById('playerTwo')
-        const playerTurnDiv = document.querySelector('.turn')
-        players[0].name = playerOne.value
-        players[1].name = playerTwo.value
-        playerTurnDiv.textContent = `${playerOne.value}'s turn`
+        game.players[0].name = playerOne.value
+        game.players[1].name = playerTwo.value
+        heading.textContent = `${playerOne.value}'s turn`
     }
 
-    const switchPlayerTurn = () => {
-        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    function boardClickHandler(e) {
+        const cellNum = e.target.dataset.num
+        const board = game.getBoard()
+        console.log(cellNum)
+
+        if (!cellNum) return
+
+        const gameState = game.playRound(cellNum)
+
+        if (gameState === "win") {
+            console.log('win')
+            const activePlayer = game.getActivePlayer().name
+            heading.textContent = `${activePlayer} WON!!!`
+            boardDisplay.removeEventListener('click', boardClickHandler)
+            boardDisplay.textContent = ''
+
+            updateBoard(board)
+            return 
+        } else if (gameState === "draw") {
+            console.log('draw')
+            heading.textContent = "It's a Draw!"
+            boardDisplay.removeEventListener('click', boardClickHandler)
+            boardDisplay.textContent = ''
+
+            updateBoard(board)
+            return
+        } 
+        console.log('no win, no draw')
+
+        updateScreen()
     }
 
-    const getActivePlayer = () => activePlayer;
-
-    const checkForWin = () => {
-        let rowCount = 0
-        let columnOneCount = 0
-        let columnTwoCount = 0
-        let columnThreeCount = 0
-        let diagTopLeftCount = 0
-        let diagBotLeftCount = 0
-        const token = getActivePlayer().token
-        for (let i = 0; i < 3; i++) {
-            rowCount = 0
-            for (let j = 0; j < 3; j++) {
-                if (board.getBoard()[i][j].getValue() === token) {
-                    rowCount++
-                    switch (j) {
-                        case (0):
-                            columnOneCount++
-                            if (i === 0) {
-                                diagTopLeftCount++
-                            } else if (i === 2) {
-                                diagBotLeftCount++
-                            }
-                            break;
-                        case (1):
-                            columnTwoCount++
-                            if (i === 1) {
-                                diagTopLeftCount++
-                                diagBotLeftCount++
-                            }
-                            break;
-                        case (2):
-                            columnThreeCount++
-                            if (i === 0) {
-                                diagBotLeftCount++
-                            } else if (i === 2) {
-                                diagTopLeftCount++
-                            } 
-                            break; 
-                    }
-                    if (rowCount === 3 ||
-                        columnOneCount === 3 ||
-                        columnTwoCount === 3 ||
-                        columnThreeCount === 3 ||
-                        diagTopLeftCount === 3 ||
-                        diagBotLeftCount === 3
-                    ) {
-                        return true
-                    }
-                }
-            }
-        }
-        return false
+    const toggleMenu = () => {
+        const form = document.querySelector('.names')
+        form.classList.toggle('hidden')
     }
-    
-    const checkForDraw = () => {
-        let count = 0
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                if (board.getBoard()[i][j].getValue() !== "") {
-                    count++
-                }
-            }
-        }
-        if (count === 9) {
-            return true
-        }
-        return false
-    }
-
-    const playRound = (row, column) => {
-        if (row > 2 || column > 2 || row < 0 || column < 0) {
-            return false
-        }
-        const mark = board.makeMove(row, column, getActivePlayer().token);
-        
-        if (!mark) {
-            return false
-        }
-
-        if (checkForWin()) {
-            return "win"
-        }
-
-        if (checkForDraw()) {
-            return "draw"
-        }
-
-        switchPlayerTurn();
-
-        return false
-    };
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -192,83 +253,18 @@ function GameController(
 
     const submit = document.querySelector('.submit')
     submit.addEventListener('click', handleSubmit)
+    boardDisplay.addEventListener('click', boardClickHandler)
+    updateScreen()
 
     return {
-        playRound,
-        getActivePlayer,
-        getBoard: board.getBoard,
-        handleSubmit
-    }
-}
-
-function ScreenController() {
-    const game = GameController();
-    const playerTurnDiv = document.querySelector('.turn')
-    const boardDiv = document.querySelector('.board')
-
-    const updateBoard = (board) => {
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                const cellButton = document.createElement('button')
-                cellButton.classList.add('cell')
-                if (board[i][j].getValue() === 'X') {
-                    cellButton.classList.add('X')
-                } else if (board[i][j].getValue() === 'O') {
-                    cellButton.classList.add('O')
-                }
-                cellButton.dataset.row = i
-                cellButton.dataset.column = j
-                cellButton.textContent = board[i][j].getValue()
-                boardDiv.appendChild(cellButton)
-            }
-        }
+        game,
+        updateBoard,
+        updateScreen
     }
 
-    const updateScreen = () => {
-        boardDiv.textContent = ''
-
-        const board = game.getBoard()
-        const activePlayer = game.getActivePlayer()
-
-        playerTurnDiv.textContent = `${activePlayer.name}'s turn`
-
-        updateBoard(board)
-    }
-
-    function clickHandlerBoard(e) {
-        const selectedRow = e.target.dataset.row
-        const selectedColumn = e.target.dataset.column
-        const board = game.getBoard()
-        if (!selectedRow || !selectedColumn) return
-
-        const gameState = game.playRound(selectedRow, selectedColumn)
-        if (gameState === "win") {
-            activePlayer = game.getActivePlayer()
-            playerTurnDiv.textContent = `${activePlayer.name} WON!!!`
-            boardDiv.removeEventListener('click', clickHandlerBoard)
-            boardDiv.textContent = ''
-
-            updateBoard(board)
-            return 
-        } else if (gameState === "draw") {
-            playerTurnDiv.textContent = "It's a Draw!"
-            boardDiv.removeEventListener('click', clickHandlerBoard)
-            boardDiv.textContent = ''
-
-            updateBoard(board)
-            return
-        } 
-
-        updateScreen()
-    }
-
-    boardDiv.addEventListener('click', clickHandlerBoard)
-
-    updateScreen()
 }
 
 ScreenController()
-
 
 const reset = document.querySelector('.reset')
 reset.addEventListener('click', (e) => {
